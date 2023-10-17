a1fe8a1d659ab5dcea649b42504831b6db1a8d18
jambrosekc <146766818+jambrosekc@users.noreply.github.com>
2023-10-17 15:34:07 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,165 +1,220 @@
-body {
-  font-family: arial, sans-serif;
-  background-color: #f9f7fe !important;
-}
+    let h2 = document.querySelector("#time-display");
+    let now = new Date();
+ 
+    function formalDate(h2, now) {
+      let date = now.getDate();
+      let year = now.getFullYear();
+      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+      let day = days[now.getDay()];
+      let hours = now.getHours();
+      let minutes = now.getMinutes();
+      let months = [
+        "Jan",
+        "Feb",
+        "March",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec"
+      ];
+      let month = months[now.getMonth()];
+      let newHours = calcHours2(hours, minutes);
+      h2.innerHTML = `${day} ${newHours}`;
+    }
+    function calcHours2(hours, minutes) {
+      hours = now.getHours();
+      if (hours > 12) {
+        hours = hours - 12;
+        displayTime = `${hours}:${minutes} PM`;
+      } else {
+        hours = hours;
+        displayTime = `${hours}:${minutes} AM`;
+      }
+      return displayTime;
+    }
 
-.App {
-  max-width: 800px;
-  margin: 60px auto;
-}
+    formalDate(h2, now);
+    
 
-.Weather {
-  font-family: "Cerebri-Sans", Helvetica, Arial, sans-serif;
-  background: white;
-  border-radius: 16px;
-  box-shadow: 0px 30px 50px rgba(65, 50, 100, 0.08);
-  padding: 40px;
-  color: rgb(135, 135, 135);
-}
+    function convertToC(event) {
+      event.preventDefault();
+      console.log(event.target.id);
+      let tempDisplay = document.querySelector("#temperature");
+      console.log(tempDisplay);
+      let clickC = document.querySelector("#celcius");
+      let clickF = document.querySelector("#fehren");
+      if (event.target.id === "celcius" && currentTempFormat !== "C") {
+        let temp = tempDisplay.innerHTML;
+        tempDisplay.innerHTML = Math.round((temp - 32) * (5 / 9));
+        currentTempFormat = "C"
+      } else if(event.target.id === "fehren" && currentTempFormat !== "F") {
+        let temp = tempDisplay.innerHTML;
+        tempDisplay.innerHTML = Math.round((temp * 9) / 5 + 32);
+        console.log(temp);
+        currentTempFormat = "F"
+      }
+    }
 
-.Weather .logo {
-  max-width: 150px;
-  display: block;
-}
+    let currentTempFormat = "C";
+    let link = document.querySelector("#celcius");
+    link.addEventListener("click", convertToC)
+    let link2 = document.querySelector("#fehren");
+    link2.addEventListener("click", convertToC)
 
-.Weather form {
-  margin-top: 40px;
-  border-bottom: 1px solid #f9f7fe;
-  padding-bottom: 30px;
-}
 
-.Weather .search-input {
-  background: #f9f7fe;
-  border: none;
-  padding: 15px 20px;
-  display: inline-block;
-  border-radius: 6px;
-  color: rgba(39, 33, 66, 0.4);
-  font-size: 16px;
-  height: 50px;
-  line-height: 20px;
-}
+    function clickSearch(event){
+      event?.preventDefault()
+      let searchCity2 = document.querySelector("#city-input");
+      //console.log(searchCity2.value);
+      let apiCity = searchCity2.value;
+      let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+      let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${apiCity}&appid=${apiKey}`;
+      updatePage(apiUrl)
+    }
 
-.Weather .btn {
-  background: #885df1;
-  font-weight: bold;
-  font-size: 16px;
-  padding: 15px 0;
-  border: none;
-  height: 50px;
-}
 
-.Weather .search-input::placeholder {
-  line-height: 20px;
-  color: rgba(39, 33, 66, 0.4);
-}
+    let searchCity = document.querySelector("#search");
+    searchCity.addEventListener("click", clickSearch);
 
-.Weather h1 {
-  margin: 20px 0 10px 0;
-  line-height: 29px;
-  font-weight: bold;
-  font-size: 38px;
-  line-height: 48px;
-  color: #272142;
-}
 
-.Weather ul {
-  margin: 0;
-  padding: 0;
-}
+    function displayWeather2(response, tempDiv, tempdescDiv, cityDiv, humidityDiv, windDiv) {
+      let city = response.data.city.name;
+      let temperature = Math.round(response.data.list[0].main.temp-273.15);
+      let description = response.data.list[0].weather[0].description;
+      let humidity = response.data.list[0].main.humidity;
+      let windSpeed = response.data.list[0].wind.speed;
+      tempDiv.innerHTML = `${temperature}`;
+      tempdescDiv.innerHTML = `${description}`;
+      cityDiv.innerHTML = `${city}`;
+      humidityDiv.innerHTML = ` ${humidity}%`;
+      windDiv.innerHTML = ` ${windSpeed}km/h`;
+    }
 
-.Weather li {
-  font-weight: 500;
-  margin-bottom: 5px;
-  color: rgba(39, 33, 66, 0.4);
-  font-size: 16px;
-  line-height: 20px;
-  list-style: none;
-}
+    function display5DayWeatherForecast(response, day1Max, day1Min, day2Max, day2Min, day3Max, day3Min, day4Max, day4Min, day5Max, day5Min, day1, day2, day3, day4, day5, icon0, icon1, icon2, icon3, icon4, icon5) {
+      
+      var days = ['Sun','Mon','Tues','Wed','Thurs','Fri','Sat'];
 
-.humidity-perc, .wind-perc {
-  color: #f65282;
-  font-weight: normal;
-}
+      let day_1 = response.data.list[0].dt;
+      let day_2 = response.data.list[8].dt;
+      let day_3 = response.data.list[16].dt;
+      let day_4 = response.data.list[24].dt;
+      let day_5 = response.data.list[32].dt;
 
-.Weather .temperature {
-  font-weight: bold;
-  color: rgb(33, 33, 33);
-  margin-left: 5px;
-  font-weight: 400;
-  margin-left: 10px;
-  font-weight: bold;
-  font-size: 80px;
-  line-height: 1;
-}
+      let today = new Date(day_1*1000)
+      let today_plus1 = new Date(day_2*1000)
+      let today_plus2 = new Date(day_3*1000)
+      let today_plus3 = new Date(day_4*1000)
+      let today_plus4 = new Date(day_5*1000)
 
-.Weather canvas {
-  margin: 10px 0;
-}
 
-.temp-display {
-  display: flex;
-  flex-direction: row;
-}
+      var day1_ = days[today.getDay()]
+      var day2_ = days[today_plus1.getDay()]
+      var day3_ = days[today_plus2.getDay()]
+      var day4_ = days[today_plus3.getDay()]
+      var day5_ = days[today_plus4.getDay()]
 
-.Weather .units {
-  color: rgb(33, 33, 33);
-  font-size: 28px;
-  line-height: 36px;
-  font-weight: normal;
-  position: relative;
-  line-height: 1;
-}
+      let day1_Max = Math.round((response.data.list[0].main.temp_max-273.15));
+      let day1_Min = Math.round(response.data.list[0].main.temp_min-273.15);
+      let day2_Max = Math.round(response.data.list[8].main.temp_max-273.15);
+      let day2_Min = Math.round(response.data.list[8].main.temp_min-273.15);
+      let day3_Max = Math.round(response.data.list[16].main.temp_max-273.15);
+      let day3_Min = Math.round(response.data.list[16].main.temp_min-273.15);
+      let day4_Max = Math.round(response.data.list[24].main.temp_max-273.15);
+      let day4_Min = Math.round(response.data.list[24].main.temp_min-273.15);
+      let day5_Max = Math.round(response.data.list[32].main.temp_max-273.15);
+      let day5_Min = Math.round(response.data.list[32].main.temp_min-273.15);
+      day1Max.innerHTML = `${day1_Max}°`;
+      day1Min.innerHTML = `${day1_Min}°`;
+      day2Max.innerHTML = `${day2_Max}°`;
+      day2Min.innerHTML = `${day2_Min}°`;
+      day3Max.innerHTML = `${day3_Max}°`;
+      day3Min.innerHTML = `${day3_Min}°`;
+      day4Max.innerHTML = `${day4_Max}°`;
+      day4Min.innerHTML = `${day4_Min}°`;
+      day5Max.innerHTML = `${day5_Max}°`;
+      day5Min.innerHTML = `${day5_Min}°`;
+      day1.innerHTML = `${day1_}`;
+      day2.innerHTML = `${day2_}`;
+      day3.innerHTML = `${day3_}`;
+      day4.innerHTML = `${day4_}`;
+      day5.innerHTML = `${day5_}`;
 
-.Weather footer {
-  font-size: 14px;
-  margin-top: 40px;
-  line-height: 18px;
-  text-align: center;
-  color: rgba(39, 33, 66, 0.4);
-}
+      let icon0num = response.data.list[0].weather[0].icon;
+      icon0.setAttribute("src", `https://openweathermap.org/img/wn/${icon0num}@2x.png`);
 
-.Weather footer a {
-  color: #885df1;
-}
 
-.WeatherForecast {
-  text-align: center;
-  margin: 40px 0;
-  border-bottom: 1px solid #f9f7fe;
-  padding-bottom: 50px;
-}
+      let icon1num = response.data.list[0].weather[0].icon;
+      icon1.setAttribute("src", `https://openweathermap.org/img/wn/${icon1num}@2x.png`);
 
-.WeatherForecast .forecast-time {
-  font-size: 16px;
-  line-height: 20px;
-  text-align: center;
-  color: rgba(39, 33, 66, 0.4);
-}
 
-.WeatherForecast .forecast-temperature {
-  font-style: normal;
-  font-size: 18px;
-  line-height: 1.5;
-  color: #f65282;
-  font-weight: bold;
-}
+      let icon2num = response.data.list[8].weather[0].icon;
+      icon2.setAttribute("src",`https://openweathermap.org/img/wn/${icon2num}@2x.png`);
 
-.WeatherForecast .forecast-temperature-min {
-  margin-left: 10px;
-  opacity: 0.6;
-}
+      let icon3num = response.data.list[16].weather[0].icon;
+      icon3.setAttribute("src",`https://openweathermap.org/img/wn/${icon3num}@2x.png`);
 
-.Weather .WeatherForecast canvas {
-  display: block;
-  margin: 20px auto;
-}
 
-.WeatherInfo {
-  margin-top: 20px;
-}
+      let icon4num = response.data.list[24].weather[0].icon;
+      icon4.setAttribute("src",`https://openweathermap.org/img/wn/${icon4num}@2x.png`) ;
+  
+      let icon5num = response.data.list[32].weather[0].icon;
+      icon5.setAttribute("src",`https://openweathermap.org/img/wn/${icon5num}@2x.png`);
 
-.WeatherInfo .temperature-container {
-  margin-top: 30px;
-  align-items: center;
-}
+      console.log(icon0num);
+      console.log(icon1num);
+      console.log(icon2num);
+
+    }
+    
+    function updatePage(apiUrl){
+      axios.get(apiUrl).then((response) => {
+        console.log(response)
+        let tempDiv = document.querySelector("#temperature");
+        let tempdescDiv = document.querySelector("#temperature-desc");
+        let cityDiv = document.querySelector("#cityInput");
+        let humidityDiv = document.querySelector("#humidity");
+        let windDiv = document.querySelector("#wind");
+        let day1Max = document.querySelector("#day1_maxtemp");
+        let day1Min = document.querySelector("#day1_mintemp");
+        let day1 = document.querySelector("#day1");
+        let day2Max = document.querySelector("#day2_maxtemp");
+        let day2Min = document.querySelector("#day2_mintemp");
+        let day2 = document.querySelector("#day2");
+        let day3Max = document.querySelector("#day3_maxtemp");
+        let day3Min = document.querySelector("#day3_mintemp");
+        let day3 = document.querySelector("#day3");
+        let day4Max = document.querySelector("#day4_maxtemp");
+        let day4Min = document.querySelector("#day4_mintemp");
+        let day4 = document.querySelector("#day4");
+        let day5Max = document.querySelector("#day5_maxtemp");
+        let day5Min = document.querySelector("#day5_mintemp");
+        let day5 = document.querySelector("#day5_disp");
+
+        let icon0 = document.querySelector("#day0_img");
+        let icon1 = document.querySelector("#day1_img");
+        let icon2 = document.querySelector("#day2_img");
+        let icon3 = document.querySelector("#day3_img");
+        let icon4 = document.querySelector("#day4_img");
+        let icon5 = document.querySelector("#day5_img");
+
+        console.log(day2Max);
+
+
+        displayWeather2(response, tempDiv, tempdescDiv, cityDiv, humidityDiv, windDiv)
+        display5DayWeatherForecast(response, day1Max, day1Min, day2Max, day2Min, day3Max, day3Min, day4Max, day4Min, day5Max, day5Min, day1, day2, day3, day4, day5, icon0, icon1, icon2, icon3, icon4, icon5)
+      }).catch(err => {
+        console.log("error")
+        let searchCity2 = document.querySelector("#city-input");
+        error404(err, searchCity2)
+      });
+
+    }
+    function error404(err, searchCity2) {
+      alert(`${searchCity2.value}: ${err.response.data.message}`)
+      console.log("how is this function getting searchCity2?");
+    }
